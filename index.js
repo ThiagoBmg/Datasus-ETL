f45390247b424136ee840f218ff2e46b9c7daa3e
ThiagoBmg <81631947+ThiagoBmg@users.noreply.github.com>
2021-07-09 11:48:15 -0300

    Update index.js
    melhorando código

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const ETL =  require('./modules/etl');
 
-
 //para chamar um novo processo de ETL com um arquivo da base do datasus
 // const exemplo = new ETL("./data/{nome do arquivo}.csv")
 const tb_estabelecimento = new ETL("./data/tbEstabelecimento.csv"),
       tb_tipoUnidade = new ETL("./data/tbTipoUnidade.csv");
-//console.log(tb_estabelecimento.d_C.slice(0,1)); // amostra de dados
 
 const estabelecimentos = tb_estabelecimento.d_C.filter((value)=> !value.CO_MOTIVO_DESAB);
 const estabelecimentos_desativados = tb_estabelecimento.d_C.filter((value)=> !!value.CO_MOTIVO_DESAB);
@@ -14,15 +12,10 @@ console.log(`\b Quantidade de estabelecimentos (total): ${tb_estabelecimento.d_C
 console.log(`\b Quantidade de estabelecimentos desativados: ${estabelecimentos_desativados.length}`);
 console.log(`\b Quantidade de estabelecimentos ativos: ${estabelecimentos.length}`);
 
-//carregando nova consulta
-//const tb_tipoUnidade = new ETL("./data/tbTipoUnidade.csv");
-//console.log(tb_tipoUnidade.d_C.slice(0,1)); // amostra de dadoos
-
-var m_tipo = [0,0] ; // variavel que vai guardar o tipo com mais unidades
+var m_tipo = [0,0] ; // variavel responsável por guardar o tipo de unidade com mais estabelecimentos
 tb_tipoUnidade.d_C.forEach((value,index)=>{
 	var result = tb_estabelecimento.d_C.filter(obj=> obj.TP_UNIDADE == value.CO_TIPO_UNIDADE);
 	tb_tipoUnidade.d_C[index] = {...tb_tipoUnidade.d_C[index], "quantidade_estabelecimentos":result.length};
 	if (m_tipo[1]<result.length) m_tipo = [index, result.length]
 });
-//console.log(tb_tipoUnidade.d_C);
 console.log(`\b Tipo de Unidade com mais estabelecimentos: ${tb_tipoUnidade.d_C[m_tipo[0]].DS_TIPO_UNIDADE} \n Quantidade: ${tb_tipoUnidade.d_C[m_tipo[0]].quantidade_estabelecimentos}`);
